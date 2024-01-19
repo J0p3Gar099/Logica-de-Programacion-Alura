59805a389ccfec0badc9b7c56226b56555bc121b
JopeGaro <josepedrogr.99@gmail.com>
2024-01-19 16:09:26 -0600

    desafio 1

---
+++ desafio 01/main.js
@@ -0,0 +1,8 @@
+alert("Bienvenida y bievenido a nuestro sitio web!");
+let nombre = prompt("¿Cual es tu nombre?");
+let edad = prompt("¿Cual es tu edad?");
+/* let  numeroDeVentas = 50;
+let saldoDisponible = 1000;
+let  mensajeDeError = "¡Error! Completa todos los campos" */
+/* alert(mensajeDeError) */
+alert(edad>=18 ? `Hola, ${nombre}! tu edad es  ${edad}` : `Losinto, ${nombre}! pero eres menor`)
