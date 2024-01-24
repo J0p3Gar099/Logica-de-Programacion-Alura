040aaa3e09f25603afae343d9a93c943d73f2d0c
JopeGaro <josepedrogr.99@gmail.com>
2024-01-24 14:25:35 -0600

    se completo el desafio 03 del curso

---
+++ desafio 03/main.js
@@ -0,0 +1,31 @@
+/* 1 Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número. */
+let contador = 1
+while(contador<11){
+    console.log(contador);
+    contador++;
+}
+
+/* 2 Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número. */
+let contador1 = 10
+while(contador1>=0){
+    console.log(contador1);
+    contador1--;
+}
+
+/* 3 Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador. */
+let numero = prompt("Introducce un numero");
+let c=0;
+while(c<= numero){
+    console.log(c);
+    c++
+}
+
+/* 4 Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador. */
+
+let numero1 = prompt("Introducce un numero");
+let c1=0;
+while(numero1>= c1){
+    console.log(numero1);
+    numero1--
+
+}
